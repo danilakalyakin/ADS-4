cde9931eab51cb2290b5240430c4cfed116ee62c
Danil Kalyakin <danila.las.vegas@gmail.com>
2024-03-17 16:22:44 +0300

    .

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,10 +18,10 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-	for (int i = 0; i < len-1; i++) {
-		for (int j = len-1; j > i; j--) {
-			if (arr[i] + arr[j] == value) {
-				count = count + 1;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
+        count = count + 1;
 			}
 		}
 	}
@@ -31,19 +31,19 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
 	for (int i = 0; i < len - 1; i++) {
-		int leftInd = i; 
+		int leftInd = i;
     int rightInd = len;
 		while (leftInd < rightInd-1) {
 			int middle = (leftInd + rightInd) / 2;
 			if (arr[i] + arr[middle] == value) {
 				count = count + 1;
 				int j = middle+1;
-				while (arr[i] + arr[j] == value && j <rightInd) {
+				while (arr[i] + arr[j] == value && j < rightInd) {
 					count = count + 1;
 					j = j+1;
 				}
 				j = middle - 1;
-				while (arr[i] + arr[j] == value && j>leftInd) {
+				while (arr[i] + arr[j] == value && j > leftInd) {
 					count = count + 1;
 					j = j - 1;
 				}
